40a455b53d643d75b8a3b75b0932ace44a1ccaf0
erkuysal <synthaine@outlook/com>
2023-10-18 22:26:17 +0300

    v0.1.1

--- website/__init__.py
+++ website/__init__.py
@@ -2,6 +2,12 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__ , template_folder='templates')
-    app.config['SECRET_KEY'] = 'useahashedkeyforsec'
+    app.config['SECRET_KEY'] = 'aabbccddeeffgg'
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
 
     return app
