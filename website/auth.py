0a58428400f9fdd9eb3bc4e83c838b66870a3cd5
erkuysal <synthaine@outlook/com>
2023-10-24 20:00:50 +0300

    get/post methods added to routes

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint , render_template , request
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login' , methods=['GET','POST'])
 def login():
     return render_template('login.html')
 
@@ -10,12 +10,12 @@ def login():
 def logout():
     return '<p> logout </p>'
 
-@auth.route('/register')
+@auth.route('/register' , methods=['GET','POST'])
 def register():
     if request == 'POST':
         email = request.form.get('email')
         username = request.form.get('username')
         password = request.form.get('password')
         password_c = request.form.get('password_c')
-        
+
     return render_template('register.html')
